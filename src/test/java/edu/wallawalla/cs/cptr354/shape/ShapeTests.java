162424d5351a20e4c903e1258d584506ace7c6e8
henswi <henswi@cs.wallawalla.edu>
2021-10-08 21:10:02 +0000

    Add directories for Java shapes example

---
+++ src/test/java/edu/wallawalla/cs/cptr354/shape/ShapeTests.java
@@ -0,0 +1,14 @@
+package edu.wallawalla.cs.cptr354.shape;
+
+import static org.junit.Assert.assertEquals;
+import java.io.StringReader;
+import org.junit.Test;
+
+public class ShapeTests {
+  
+  @Test
+  public void testSquare() throws Exception{
+
+  }
+
+}
